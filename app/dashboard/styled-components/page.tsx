623aaf4507a63259b593bef5da05142935e6b27e
gucudmitriy <dmitiy.g@helium10.com>
2024-03-14 21:47:04 +0200

    [styled-components] add description

--- app/dashboard/styled-components/page.tsx
+++ app/dashboard/styled-components/page.tsx
@@ -11,7 +11,7 @@ export default function Page() {
           Right
         </Description>
 
-        <Description>Styled Components have been configured</Description>
+        <Description>Styled Components have been configured!!!</Description>
       </Flex>
     </div>
   );
